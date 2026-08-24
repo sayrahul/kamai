c83ec9d9daadf7a84ad5d5bf5da4fcd8e9f75e46
Rahul Jadhav <rahuljadhav44@gmail.com>
2026-08-24 22:18:44 +0530

    chore(scripts): add push helper scripts for quick remote sync

---
+++ push.bat
@@ -0,0 +1,4 @@
+@echo off
+set "PATH=%LOCALAPPDATA%\Programs\MinGit\cmd;%LOCALAPPDATA%\Programs\nodejs;%PATH%"
+git push origin main
+pause
---
+++ push.ps1
@@ -0,0 +1,2 @@
+$env:PATH = "$env:LOCALAPPDATA\Programs\MinGit\cmd;$env:LOCALAPPDATA\Programs\nodejs;$env:PATH"
+git push origin main
